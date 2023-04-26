4bff65cfc5b4ff5aa65d5005c5aae6ca8bea92ff
Ali Ebrahimi <aliebrahimi6739@gmail.com>
2023-04-27 03:11:33 +0430

    add threadpool to face encoder

--- src/detection/face_encoder.py
+++ src/detection/face_encoder.py
@@ -1,6 +1,9 @@
+import concurrent.futures
 import face_recognition
 
-
 class FaceEncoder:
+    def __init__(self):
+        self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=4)
+
     def encode_image(self, image):
-        return face_recognition.face_encodings(image)
+        return self.executor.submit(face_recognition.face_encodings, image)
