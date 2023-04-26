ad3decf37af2b256290cb693c89c9de0895021db
Ali Ebrahimi <aliebrahimi6739@gmail.com>
2023-04-27 03:36:59 +0430

    add thread pool

--- src/detection/face_encoder.py
+++ src/detection/face_encoder.py
@@ -1,12 +1,11 @@
-import threading
-
+import multiprocessing
 import face_recognition
 
 
 class FaceEncoder:
-    def __init__(self):
-        self.lock = threading.Lock()
+    def __init__(self, num_processes=10):
+        self.pool = multiprocessing.Pool(num_processes)
 
     def encode_image(self, image):
-        with self.lock:
-            return face_recognition.face_encodings(image)
+        result = self.pool.apply_async(face_recognition.face_encodings, (image,))
+        return result.get()
